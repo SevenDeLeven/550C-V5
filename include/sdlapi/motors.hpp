ca413fb4a8f619db18da60d8af10a174b0719276
SevenDeLeven <sevendeleven.gaming@gmail.com>
2018-11-01 13:56:06 -0400

    First compile
    Began work on library

---
+++ include/sdlapi/motors.hpp
@@ -0,0 +1,18 @@
+#ifndef SDLAPI_MOTORS_HPP
+#define SDLAPI_MOTORS_HPP
+
+#include "../pros/motors.hpp"
+
+namespace sdl {
+
+class motorgroup {
+private:
+
+public:
+  motorgroup();
+  virtual ~motorgroup();
+};
+
+}
+
+#endif
