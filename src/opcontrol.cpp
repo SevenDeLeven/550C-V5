5cac711b294c59b334e2c20b1d990daacfbd9ebe
SevenDeLeven <sevendeleven.gaming@gmail.com>
2018-11-08 14:36:39 -0500

    Reupload

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -6,19 +6,6 @@
 #define FLYWHEEL_MIDDLEFAR 2
 #define FLYWHEEL_TOPFAR 3
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	master.set_text(0,0,"FW: OFF");
@@ -44,6 +31,7 @@ void opcontrol() {
 	bool temp_alert = false;
 	int flyWheelSpeedUpdate = 0;
 	sdl::Timer flyWheelRunTimer;
+
 	while (true) {
 
 		bool flyWheelMaxCurPressed = master.get_digital(DIGITAL_X);
